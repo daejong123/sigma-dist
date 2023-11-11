b33a823ead63dec657f452131d61887cbdcf044c
lindazhong <lindazhong@lixiang.com>
2023-11-11 22:12:10 +0800

    feat: opti website

---
+++ js/chunk-2d229b98.2ef97704.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d229b98"],{df55:function(n,e,t){"use strict";t.r(e);var l=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("detail jd")])},u=[],c={},a=c,d=t("2877"),i=Object(d["a"])(a,l,u,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d229b98.2ef97704.js.map
